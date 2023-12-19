90df07378d63dbff2de9faa2366f253ec079832c
DasBaka <122975603+DasBaka@users.noreply.github.com>
2023-12-19 18:57:13 +0100

    fixed vuetify css - justify

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,36 +1,39 @@
 <template>
-<div class="queue">
+  <div class="queue">
     <v-app-bar :elevation="5">
-        <v-app-bar-title>now playing</v-app-bar-title>
-        <template v-slot:append>
-            <v-btn icon="mdi-pause"></v-btn>
-        </template>
+      <v-app-bar-title>now playing</v-app-bar-title>
+      <template v-slot:append>
+        <v-btn icon="mdi-pause"></v-btn>
+      </template>
     </v-app-bar>
     <v-list lines="one">
-        <v-list-item v-for="n in 25" :key="n">
+      <v-list-item
+        v-for="n in 25"
+        :key="n"
+        class="px-2">
+        <div class="d-flex w-100 justify-space-between pa-2">
+          <div class="d-flex ga-2 flex-grow-1">
+            <v-img
+              :max-width="50"
+              aspect-ratio="1/1"
+              cover
+              src="@/assets/daily.jpg"></v-img>
+            <div>
+              <p style="font-size: xx-small">nr. & date</p>
+              <p style="font-size: small">episode titel kk</p>
+              <p style="font-size: xx-small">dlstate&lenghth</p>
+            </div>
+          </div>
 
-            <v-row>
-                <v-col cols="2">
-                    <v-img :max-width="50" aspect-ratio="1/1" cover src="@/assets/daily.jpg"></v-img>
-                </v-col>
-                <v-col>
-                    <div style="font-size:xx-small">nr. & date</div>
-                    <div style="font-size:small">episode titel kk</div>
-                    <div style="font-size:xx-small">dlstate&lenghth</div>
-                </v-col>
-                <v-col justify="end">
-                    <v-btn icon="mdi-pan-vertical"></v-btn>
-                </v-col>
-            </v-row>
+          <v-btn icon="mdi-pan-vertical"></v-btn>
+        </div>
 
-           
-
-            <v-divider :thickness="2" class="border-opacity-50"></v-divider>
-        </v-list-item>
+        <v-divider
+          :thickness="2"
+          class="border-opacity-50"></v-divider>
+      </v-list-item>
     </v-list>
-</div>
+  </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
