b8e53b4489adde295c9e87d7f19e9b2dc583108e
justcallmeduncan <drunkenduncan@web.de>
2024-01-29 19:54:27 +0100

    Made the fucking bottom nav buttons resizable, which the shold be in the fucking first place

---
+++ rss/sample.vue
@@ -0,0 +1,262 @@
+<template>
+<main class="v-main" style="--v-layout-left: 0px; --v-layout-right: 0px; --v-layout-top: 64px; --v-layout-bottom: 56px;">
+    <div class="podcasts">
+        <header class="v-toolbar v-toolbar--density-default elevation-5 v-theme--dark v-locale--is-ltr v-app-bar" style="top: 0px; z-index: 1006; transform: translateY(0%); position: fixed; left: 0px; width: calc(100% - 0px);">
+            <!---->
+            <div class="v-toolbar__content" style="height: 64px;">
+                <!---->
+                <!---->
+                <div class="v-toolbar-title v-app-bar-title">
+                    <div class="v-toolbar-title__placeholder">
+                        <!---->
+                        podcasts</div>
+                </div>
+                <div class="v-toolbar__append">
+                    <button type="button" class="v-btn v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-tonal" aria-haspopup="dialog" aria-expanded="false">
+                        <span class="v-btn__overlay">
+                        </span>
+                        <span class="v-btn__underlay">
+                        </span>
+                        <!---->
+                        <span class="v-btn__content" data-no-activator="">
+                            add
+                            <!---->
+                            <!---->
+                        </span>
+                        <span class="v-btn__append">
+                            <i class="mdi-rss-box mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                            </i>
+                        </span>
+                        <!---->
+                    </button>
+                    <button type="button" class="v-btn v-btn--icon v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text">
+                        <span class="v-btn__overlay">
+                        </span>
+                        <span class="v-btn__underlay">
+                        </span>
+                        <!---->
+                        <span class="v-btn__content" data-no-activator="">
+                            <i class="mdi-folder-plus mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                            </i>
+                        </span>
+                        <!---->
+                        <!---->
+                    </button>
+                </div>
+            </div>
+            <!---->
+        </header>
+        <div class="v-expansion-panels v-theme--dark v-expansion-panels--variant-default">
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    🕓<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    ⚾<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    😆<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    🎵🎬📚<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    🎙️<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    🎭<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    🏁<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+            <div class="v-expansion-panel">
+                <div class="v-expansion-panel__shadow">
+                </div>
+                <button class="v-expansion-panel-title" type="button" aria-expanded="false">
+                    <span class="v-expansion-panel-title__overlay">
+                    </span>
+                    ungrouped<span class="v-expansion-panel-title__icon">
+                        <i class="mdi-chevron-down mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                    </span>
+                </button>
+                <!---->
+                <div class="v-expansion-panel-text" style="display: none;">
+                    <!---->
+                </div>
+            </div>
+        </div>
+    </div>
+    <header class="v-bottom-navigation v-bottom-navigation--active v-theme--dark v-bottom-navigation--density-default height: 150px" style="bottom: 0px; z-index: 1004; transform: translateY(0%); position: fixed; height: 56px; left: 0px; width: calc(100% - 0px);">
+        <div class="v-bottom-navigation__content">
+            <div class="d-flex w-100 justify-space-evenly">
+                <a class="v-btn v-btn--selected v-btn--active v-btn--stacked v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text d-flex" style="font-size: x-small;" href="/podcasts" value="podcasts">
+                    <span class="v-btn__overlay">
+                    </span>
+                    <span class="v-btn__underlay">
+                    </span>
+                    <!---->
+                    <span class="v-btn__content" data-no-activator="">
+                        <i class="mdi-dots-grid mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                        <span>
+                        </span>
+                    </span>
+                    <!---->
+                    <!---->
+                </a>
+                <a class="v-btn v-btn--stacked v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text" style="font-size: x-small;" href="/filters" value="filters">
+                    <span class="v-btn__overlay">
+                    </span>
+                    <span class="v-btn__underlay">
+                    </span>
+                    <!---->
+                    <span class="v-btn__content" data-no-activator="">
+                        <i class="mdi-filter-multiple mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                        <span>
+                        </span>
+                    </span>
+                    <!---->
+                    <!---->
+                </a>
+                <a class="v-btn v-btn--stacked v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text" style="font-size: x-small;" href="/profile" value="profile">
+                    <span class="v-btn__overlay">
+                    </span>
+                    <span class="v-btn__underlay">
+                    </span>
+                    <!---->
+                    <span class="v-btn__content" data-no-activator="">
+                        <i class="mdi-account mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                        <span>
+                        </span>
+                    </span>
+                    <!---->
+                    <!---->
+                </a>
+                <a class="v-btn v-btn--stacked v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text" style="font-size: x-small;" href="/player" value="player">
+                    <span class="v-btn__overlay">
+                    </span>
+                    <span class="v-btn__underlay">
+                    </span>
+                    <!---->
+                    <span class="v-btn__content" data-no-activator="">
+                        <i class="mdi-play mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                        <span>
+                        </span>
+                    </span>
+                    <!---->
+                    <!---->
+                </a>
+                <a class="v-btn v-btn--stacked v-theme--dark v-btn--density-default v-btn--size-default v-btn--variant-text" style="font-size: xx-small;" href="/queue" value="queue">
+                    <span class="v-btn__overlay">
+                    </span>
+                    <span class="v-btn__underlay">
+                    </span>
+                    <!---->
+                    <span class="v-btn__content" data-no-activator="">
+                        <i class="mdi-format-list-bulleted-square mdi v-icon notranslate v-theme--dark v-icon--size-default" aria-hidden="true">
+                        </i>
+                        <span>
+                        </span>
+                    </span>
+                    <!---->
+                    <!---->
+                </a>
+            </div>
+        </div>
+    </header>
+</main>
+</template>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -6,13 +6,14 @@
       <router-view />
       <v-bottom-navigation class="height: 150px">
         <div class="d-flex w-100  justify-space-evenly">
-        <v-btn class="d-flex" value="podcasts"  style="font-size: x-small" :to="{name: 'Podcasts'}">
+          
+        <v-btn class="d-flex flex-grow-1" value="podcasts"  style="font-size: xx-small; min-width: 0; height: 56px ;" :to="{name: 'Podcasts'}">
           <v-icon>mdi-dots-grid</v-icon>
       
           <span></span>
         </v-btn>
       
-        <v-btn value="filters" style="font-size: x-small" :to="{name: 'Filters'}">
+        <v-btn  class="d-flex flex-grow-1"  value="filters" style="font-size:xx-small; min-width: 0; height: 56px;width: auto" :to="{name: 'Filters'}">
           <v-icon>mdi-filter-multiple</v-icon>
       
           <span></span>
@@ -20,19 +21,19 @@
       
         
     
-        <v-btn  value="profile"  style="font-size: x-small" :to="{name: 'Profile'}">
+        <v-btn class="d-flex flex-grow-1"   value="profile"  style="font-size:xx-small;min-width: 0 ;height: 56px;width:calc(var(--variable-width) + 20px)" :to="{name: 'Profile'}">
           <v-icon>mdi-account</v-icon>
       
           <span></span>
         </v-btn>
     
-        <v-btn  value="player" style="font-size: x-small" :to="{name: 'Player'}">
+        <v-btn class="d-flex flex-grow-1"  value="player" style="font-size:xx-small; min-width: 0; height: 56px;width:calc(var(--variable-width) + 20px)" :to="{name: 'Player'}">
           <v-icon>mdi-play</v-icon>
       
           <span></span>
         </v-btn>
   
-        <v-btn  value="queue" style="font-size: xx-small" :to="{name: 'Queue'}">
+        <v-btn  class="d-flex flex-grow-1"  value="queue" style="font-size:xx-small; min-width: 0; height: 56px;" :to="{name: 'Queue'}">
           <v-icon>mdi-format-list-bulleted-square</v-icon>
       
           <span></span>
